e6e27e1ee4f93bde0073673b951af7409a5f5991
Carlos Marcelo <carlosmcarpes@gmail.com>
2022-05-30 20:27:56 -0300

    v01

---
+++ Controllers/TipoProdutoController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Estoque.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Estoque.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TipoProdutoController : ControllerBase
+    {
+        private static List<TipoProduto> tipoProdutos = new List<TipoProduto>();
+        private static int Id = 1;
+
+        [HttpPost]
+        public IActionResult Adicionar([FromBody] TipoProduto tipoproduto)
+        {
+            tipoproduto.id = Id++;
+            tipoProdutos.Add(tipoproduto);
+            return CreatedAtAction(nameof(recuperarPorId), new {id = tipoproduto.id },tipoproduto);
+
+        }
+
+        [HttpGet]
+        public IActionResult recuperar()
+        {
+            return Ok(tipoProdutos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult recuperarPorId(int Id)
+        {
+
+            TipoProduto tipoProduto =  tipoProdutos.FirstOrDefault(tipoproduto => tipoproduto.id == Id);
+            if(tipoProduto != null)
+            {
+                return Ok(tipoProduto);
+            }
+            return NotFound();
+
+        }
+        
+    }
+}
---
+++ Models/TipoProduto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estoque.Models
+{
+    public class TipoProduto
+    {
+        public int id {get; set;}
+
+        [Required (ErrorMessage ="descrição obrigatório")]
+        public string descricao {get; set;}
+
+        
+    }
+}
